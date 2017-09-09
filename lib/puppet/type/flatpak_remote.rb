645435fed24a2cc90b431fb170a11d0dae6589af
Bryan Wyatt <brwyatt@gmail.com>
2017-09-09 12:42:43 -0700

    Add params to flatpak_remote type

--- lib/puppet/type/flatpak_remote.rb
+++ lib/puppet/type/flatpak_remote.rb
@@ -9,6 +9,24 @@ Puppet::Type.newtype(:flatpak_remote) do
 
   ensurable
 
+  validate do
+  end
+
+  newparam(:name, :namevar => true) do
+    desc 'Name of the remote'
+    newvalues(/\A[a-zA-Z0-9.\-_]*\Z/)
+  end
+
+  newparam(:location) do
+    desc 'Location of the remote repo'
+  end
+
+  newparam(:from) do
+    desc 'If true, specifies that `location` is a repo config file, not the repo location'
+
+    defaultto false
+  end
+
 end
 
 # vim: ts=2 sts=2 sw=2 expandtab
