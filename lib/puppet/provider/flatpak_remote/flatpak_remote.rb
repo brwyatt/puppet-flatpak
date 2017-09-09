4d99f3d602bb13371af6c8ca6bb8df5757415264
Bryan Wyatt <brwyatt@gmail.com>
2017-09-09 13:55:11 -0700

    Add copyright notice to provider/type files

--- lib/puppet/provider/flatpak_remote/flatpak_remote.rb
+++ lib/puppet/provider/flatpak_remote/flatpak_remote.rb
@@ -1,3 +1,23 @@
+# Copyright
+# ---------
+#
+# Copyright 2017 Bryan Wyatt, unless otherwise noted.
+#
+# This file is part of brwyatt-flatpak.
+#
+# brwyatt-flatpak is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# brwyatt-flatpak is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with brwyatt-flatpak.  If not, see <http://www.gnu.org/licenses/>.
+
 Puppet::Type.type(:flatpak_remote).provide(:flatpak_remote) do
 
   commands :flatpak => '/usr/bin/flatpak'
