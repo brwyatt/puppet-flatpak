4a5564c7e01e5873223067785eee7e5ccd813f78
Bryan Wyatt <brwyatt@gmail.com>
2019-01-05 11:29:12 -0800

    Add repo config with ini_setting

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,7 +4,38 @@
 #   include ::flatpak::config
 #
 class flatpak::config (){
-  # Nothing here ... yet
+  $repo_config_path = '/var/lib/flatpak/repo/config'
+
+  file { 'Flatpak repo config':
+    path  => $repo_config_path,
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
+
+  Ini_setting {
+    path      => $repo_config_path,
+    section   => 'core',
+    show_diff => true,
+  }
+
+  ini_setting { 'Flatpak repo config - core:repo_version':
+    ensure    => present,
+    setting   => 'repo_version',
+    value     => '1',
+  }
+
+  ini_setting { 'Flatpak repo config - core:mode':
+    ensure  => present,
+    setting => 'mode',
+    value   => 'bare-user-only',
+  }
+
+  ini_setting { 'Flatpak repo config - core:min-free-space-size':
+    ensure  => present,
+    setting => 'min-free-space-size',
+    value   => '500MB',
+  }
 }
 
 # vim: ts=2 sts=2 sw=2 expandtab
