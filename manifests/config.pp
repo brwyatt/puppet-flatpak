c7d95a856cb661e120205690bf6b0efa6093dd3a
Bryan Wyatt <brwyatt@gmail.com>
2019-01-05 11:15:54 -0800

    Add repo config with concat

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,35 +6,24 @@
 class flatpak::config (){
   $repo_config_path = '/var/lib/flatpak/repo/config'
 
-  file { 'Flatpak repo config':
-    path  => $repo_config_path,
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
+  concat { 'Flatpak repo config':
+    ensure => present,
+    path   => $repo_config_path,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
 
-  Ini_setting {
-    path      => $repo_config_path,
-    section   => 'core',
-    show_diff => true,
-  }
-
-  ini_setting { 'Flatpak repo config - core:repo_version':
-    ensure    => present,
-    setting   => 'repo_version',
-    value     => '1',
-  }
-
-  ini_setting { 'Flatpak repo config - core:mode':
-    ensure  => present,
-    setting => 'mode',
-    value   => 'bare-user-only',
-  }
-
-  ini_setting { 'Flatpak repo config - core:min-free-space-size':
-    ensure  => present,
-    setting => 'min-free-space-size',
-    value   => '500MB',
+  concat::fragment { 'Flatpak repo config core section':
+    target  => $repo_config_path,
+    order   => '01',
+    content => join([
+      '[core]',
+      'repo_version=1',
+      'mode=bare-user-only',
+      'min-free-space-size=500MB',
+      '',
+    ], "\n")
   }
 }
 
