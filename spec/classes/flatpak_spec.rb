930abe05d536c14c1f37eee2677e58fbb2fd165d
Bryan Wyatt <brwyatt@gmail.com>
2020-10-10 16:06:29 -0700

    Update tests for OS support for repo management
    ... really just for testing the automatic setting of the `manage_repo`
    parameter.

--- spec/classes/flatpak_spec.rb
+++ spec/classes/flatpak_spec.rb
@@ -1,19 +1,24 @@
 require 'spec_helper'
 
 describe 'flatpak' do
-  on_supported_os(facterversion: '2.4').each do |os, os_facts|
+  on_supported_os(facterversion: '2.4').each do |os, facts|
     context "on #{os}" do
-      let(:facts) { os_facts }
+      let(:facts) { facts }
 
       it { is_expected.to compile }
       it { is_expected.to contain_package('flatpak') }
-      it { is_expected.to contain_class('apt') }
 
-      context 'with repo_file_name' do
-        let(:params) { { 'repo_file_name' => 'TEST' } }
+      if facts[:osfamily] == 'Debian' then
+      	it { is_expected.to contain_class('apt') }
 
-        it { is_expected.to contain_apt__source('TEST') }
-        it { is_expected.to contain_file('/etc/apt/sources.list.d/TEST.list') }
+        context 'with repo_file_name' do
+          let(:params) { { 'repo_file_name' => 'TEST' } }
+
+          it { is_expected.to contain_apt__source('TEST') }
+          it { is_expected.to contain_file('/etc/apt/sources.list.d/TEST.list') }
+        end
+      elsif facts[:osfamily] == 'RedHat' then
+        it { is_expected.to_not contain_class('apt') }
       end
     end
   end
